9bcf1eee279b4fa6623d4ae0916ea8724adf21b0
HypericatsFramework <charlesolivier.malo@gmail.com>
2024-09-09 15:57:21 -0400

    Added basic stuff

---
+++ src/HeadlessInstance.java
@@ -0,0 +1,16 @@
+import Networking.NetworkHandler;
+
+public class HeadlessInstance {
+
+    private final NetworkHandler network;
+
+    public HeadlessInstance() {
+        network = new NetworkHandler();
+    }
+
+    public void connect(String ip) {
+
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import Networking.NetworkHandler;
 
+public class Main {
+    //make absolutely nothing static except for final variables in order to allow for multi clients
+
+    public static void main(String[] args) {
+        HeadlessInstance headless = new HeadlessInstance();
+        headless.connect("127.0.0.1");
     }
 }
